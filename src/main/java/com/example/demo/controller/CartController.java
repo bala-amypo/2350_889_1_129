46511bccc5ce588531825a769e3df8f533997f8e
bala-amypo <bala@amypo.in>
2025-12-19 08:03:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,9 +1,3 @@
-package com.example.demo.controller;
-
-import com.example.demo.model.Cart;
-import com.example.demo.service.impl.CartServiceImpl;
-import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/api/carts")
 public class CartController {
@@ -12,15 +6,5 @@ public class CartController {
 
     public CartController(CartServiceImpl cartService) {
         this.cartService = cartService;
-    }
-
-    @PostMapping("/user/{userId}")
-    public Cart create(@PathVariable Long userId) {
-        return cartService.createCart(userId);
-    }
-
-    @GetMapping("/user/{userId}")
-    public Cart getActive(@PathVariable Long userId) {
-        return cartService.getActiveCartForUser(userId);
     }
 }
