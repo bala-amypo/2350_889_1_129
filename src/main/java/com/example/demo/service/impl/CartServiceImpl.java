f208af23a7f4561d6e6f54da218155e08f1105aa
bala-amypo <bala@amypo.in>
2025-12-19 08:15:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CartServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CartServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demo.service.impl;
 import com.example.demo.model.Cart;
 import com.example.demo.repository.CartRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
 @Service
 public class CartServiceImpl {
 
