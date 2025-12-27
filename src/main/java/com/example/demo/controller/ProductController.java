a17b2fc56d8ebd0071e99c7319b2f50c117a8ce5
bala-amypo <bala@amypo.in>
2025-12-27 05:12:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -36,7 +36,6 @@ public class ProductController {
     }
     
     @GetMapping("/{id}")
-    @PreAuthorize
     @Operation(summary = "Get product by ID")
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
         Product product = productService.getProductById(id);
